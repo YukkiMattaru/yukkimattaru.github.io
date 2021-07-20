d2b408b39cc12131a50a5b72e17bb1e322f08382
gajdukov <gajdukov@web-bee.ru>
2021-07-21 00:18:37 +0300

    fix: added docs

--- webpack.prod.js
+++ webpack.prod.js
@@ -23,7 +23,7 @@ module.exports = {
     ],
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     filename: "index.bundle.js",
   },
   plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
